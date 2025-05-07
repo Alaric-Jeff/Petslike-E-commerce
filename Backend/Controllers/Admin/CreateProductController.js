f4a0f2b7dffc950d847dec2b7c2c7dc01c3459fe
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-07 15:41:14 +0800

    updated create product controller

--- Backend/Controllers/Admin/CreateProductController.js
+++ Backend/Controllers/Admin/CreateProductController.js
@@ -7,16 +7,29 @@ const CreateProductController = async (req, res) => {
 
     if(!productName || !productPrice || !productQuantity || !meatType || !lifeStage){
         logger.debug("Incomplete fields recieved in create product controller. "); 
-        return res.status(400).json({message: "Incomplete fields"})
+        return res.status(400).json({
+            success: false,
+            message: "Incomplete fields",
+            error: "Missing required fields"
+        })
     }
 
     try{
         await CreateProduct(productName, productPrice, productQuantity, meatType, lifeStage);
         logger.info(`successfully created product ${productName}`)
-        return res.status(200).json({message: "Succesfully created product"})
+
+        return res.status(200).json({
+            success: true,
+            message: "Succesfully created product"
+        })
+        
     }catch(err){
         logger.error("Error in creating product contoller, reason: ", err)
-        return res.status(500).json({message: "Internal server error"})
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: err.message
+        })
     }
 };
 
