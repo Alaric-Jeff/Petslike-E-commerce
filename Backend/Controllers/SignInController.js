db3a39a3aaf795030abae242c4ef62737b41e6b5
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-07 15:56:04 +0800

    added sign in controller

--- Backend/Controllers/SignInController.js
+++ Backend/Controllers/SignInController.js
@@ -1,1 +1,34 @@
-import UserModel from "../Models/UserModel.js";
+import { log } from "winston";
+import SignInService from "../Services/SignInService.js";
+import logger from "../Utils/logger.js";
+
+const SignInController = async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        logger.debug("Incomplete fields");
+        return res.status(400).json({
+            success: false,
+            message: "Incomplete fields",
+            error: "Missing required fields"
+        })
+    }
+
+    try{
+        await SignInService(email, password);
+        logger.info("Sign in successful");
+        return res.status(200).json({
+            success: true,
+            message: "Sign in successful"
+        })
+    }catch(err){
+        logger.error("Error in SignInController, reason: ", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: err.message
+        })
+    }
+};
+
+export default SignInController;
