6367c7ca04c0df9f62ea19d21d8a6398684b1770
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-08 22:58:17 +0800

    added get products by category

---
+++ Backend/Services/GetProductsByCategory.js
@@ -0,0 +1,18 @@
+import ProductModel from "../Models/ProductModel.js";
+import logger from "../Utils/logger.js";
+
+async function GetProductsByCategory(category){
+    try{
+        const products = await ProductModel.findAll({
+            where: {
+                category: category
+            }
+        });
+        return products;
+    }catch(err){
+        logger.error("Error in GetProductsByCategory, reason: ", err);
+        throw err;
+    }
+}
+
+export default GetProductsByCategory;
