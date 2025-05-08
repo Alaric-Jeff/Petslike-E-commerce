39e9b6845503aedc3037a848bf53ead1056916f0
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-08 22:56:28 +0800

    added get all products

---
+++ Backend/Services/GetAllProducts.js
@@ -0,0 +1,14 @@
+import ProductModel from "../Models/ProductModel.js";
+import logger from "../Utils/logger.js";
+
+async function GetAllProducts(){
+   try{
+    const Products = await ProductModel.findAll();
+    return Products;
+   }catch(err){
+    logger.error("Error occured in fetching orders in services, reason: ", err)
+    throw err;
+   }
+};
+
+export default GetAllProducts;
