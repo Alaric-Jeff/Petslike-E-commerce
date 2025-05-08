fb58e21a3dd0f399e792b23f30907762c8c7a994
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-08 22:54:41 +0800

    added get all users

---
+++ Backend/Services/GetAllUsers.js
@@ -0,0 +1,14 @@
+import User from "../Models/User.js";
+import logger from "../Utils/logger.js";
+
+async function GetAllUsers(){
+    try{
+        const users = await User.findAll();
+        return users;
+    }catch(err){
+        logger.error("Error in GetAllUsers, reason: ", err);
+        throw err;
+    }
+}
+
+export default GetAllUsers;
