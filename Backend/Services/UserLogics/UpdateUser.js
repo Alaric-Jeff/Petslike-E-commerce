9e08d227ee67e1760d600773bc9c205b58b93a68
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-09 20:19:20 +0800

    fixed routes

--- Backend/Services/UserLogics/UpdateUser.js
+++ Backend/Services/UserLogics/UpdateUser.js
@@ -1,5 +1,4 @@
-import { log } from "winston";
-import UserModel from "../../Models/UserModel.js";
+import UserModel from '../../Models/UserModel.js';
 import logger from "../../Utils/logger.js";
 
 async function UpdateUser(userId, FormData){
