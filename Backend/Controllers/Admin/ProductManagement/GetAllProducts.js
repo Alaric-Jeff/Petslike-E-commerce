a57b0b313f026cc88bfba84255a24a8a5690d7d8
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-14 06:54:40 +0800

    added get all products

---
+++ Backend/Controllers/Admin/ProductManagement/GetAllProducts.js
@@ -0,0 +1,21 @@
+import GetAllProducts from '../../../Services/ProductLogics/GetAllProducts.js'
+import logger from '../../../Utils/logger.js'
+
+const GetAllProductsController = async (req, res) => {
+    try{
+        const products = await GetAllProducts();
+        logger.info("Fetched all products");
+        return res.status(200).json({
+            success: true,
+            message: "Get Products Success",
+            products
+        })
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+};
+
+export default GetAllProductsController;
