7694da46da0354254943098f61aa0992b7c06bf5
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-29 13:31:38 +0800

    added rate limit in auths

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import SignUpController from '../Controllers/Users/SignUpController.js';
 import SignInController from '../Controllers/Users/SignInController.js';
+import { authLimiter, signUpLimiter} from '../Middleware/rateLimitMiddleware.js';
 
 const router = express.Router()
 
 router
-    .post('/sign-up', SignUpController)
-    .post('/sign-in', SignInController);
+    .post('/sign-up', signUpLimiter, SignUpController)
+    .post('/sign-in', authLimiter, SignInController);
 
 export default router;
