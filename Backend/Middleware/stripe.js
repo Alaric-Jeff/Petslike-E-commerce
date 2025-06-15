e6d696b6dc3ccc076607b03dfd1c11c691a6ede5
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-06-15 14:56:11 +0800

    added stripe config

---
+++ Backend/Middleware/stripe.js
@@ -0,0 +1,10 @@
+import stripe, { Stripe } from 'stripe'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_KEY, { 
+    apiVersion: '2025-05-28'
+})
+
+export default stripe
